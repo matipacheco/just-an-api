771b1dca62712d9f54751773353387306c3dc046
Mati <matipacheco@gmail.com>
2019-03-14 17:39:02 -0300

    adapting the tests to the new serializer

--- app/serializers/pokemon_serializer.rb
+++ app/serializers/pokemon_serializer.rb
@@ -1,3 +1,3 @@
 class PokemonSerializer < ActiveModel::Serializer
-  attributes :id, :identifier, :weight, :height, :base_experience
+  attributes :id, :identifier, :height, :weight, :base_experience
 end
--- spec/controllers/api/v1/pokemon_controller_spec.rb
+++ spec/controllers/api/v1/pokemon_controller_spec.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'spec_helper'
 
 describe Api::V1::PokemonController, type: :api do
-  context 'Testing GET method' do
+  context 'Bulbasaur, I choose you!' do
     before do
       get '/api/v1/pokemon/1'
     end
@@ -13,9 +13,7 @@ describe Api::V1::PokemonController, type: :api do
           "identifier": "bulbasaur",
           "height": 7,
           "weight": 69,
-          "base_experience": 64,
-          "created_at": "2019-03-10T18:33:38.000Z",
-          "updated_at": "2019-03-10T18:33:38.000Z"
+          "base_experience": 64
       }
 
       expect(last_response.body).to eq(bulbasaur.to_json)
