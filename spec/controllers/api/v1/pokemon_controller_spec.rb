5fc431ed9a53a9d71b1b694e52602055454f062d
Mati <matipacheco@gmail.com>
2019-03-14 18:20:23 -0300

    DELETE test

--- spec/controllers/api/v1/pokemon_controller_spec.rb
+++ spec/controllers/api/v1/pokemon_controller_spec.rb
@@ -2,46 +2,14 @@ require 'json'
 require 'spec_helper'
 
 describe Api::V1::PokemonController, type: :api do
-  context 'Bulbasaur, I choose you!' do
-    before do
-      get '/api/v1/pokemon/1'
-    end
+  bulbasaur = {
+      "id": 1,
+      "identifier": "bulbasaur",
+      "height": 7,
+      "weight": 69,
+      "base_experience": 64
+  }
 
-    it 'should respond with Bulbasaurs data' do
-      bulbasaur = {
-          "id": 1,
-          "identifier": "bulbasaur",
-          "height": 7,
-          "weight": 69,
-          "base_experience": 64
-      }
-
-      expect(last_response.body).to eq(bulbasaur.to_json)
-    end
-  end
-end
-
-describe Api::V1::PokemonController, type: :api do
-  context 'Bulbasaur, I... update you (?)' do
-    before do
-      put '/api/v1/pokemon/1', pokemon: { "height": 1 }
-    end
-
-    it 'should respond with Bulbasaurs updated data' do
-      bulbasaur = {
-          "id": 1,
-          "identifier": "bulbasaur",
-          "height": 1,
-          "weight": 69,
-          "base_experience": 64
-      }
-
-      expect(last_response.body).to eq(bulbasaur.to_json)
-    end
-  end
-end
-
-describe Api::V1::PokemonController, type: :api do
   suki = {
       "identifier": 'Suki',
       "height": 1,
@@ -49,12 +17,59 @@ describe Api::V1::PokemonController, type: :api do
       "base_experience": 0
   }
 
+  ############################################################
+  # GET test
+  ############################################################
+  context 'Bulbasaur, I choose you!' do
+    before do
+      get '/api/v1/pokemon/1'
+    end
+
+    it 'should respond with Bulbasaurs data' do
+      expect(last_response.body).to eq(bulbasaur.to_json)
+    end
+  end
+
+  ############################################################
+  # PUT test
+  ############################################################
+  context 'Bulbasaur, I... update you (?)' do
+    before do
+      put '/api/v1/pokemon/1', pokemon: { "height": 1 }
+    end
+
+    it 'should respond with Bulbasaurs updated data' do
+      response = JSON.parse last_response.body
+      bulbasaur[:height] = 1
+
+      expect(response.to_json).to eq(bulbasaur.to_json)
+
+      bulbasaur[:height] = 7
+    end
+  end
+
+  ############################################################
+  # DELETE test
+  ############################################################
+  context 'Bulbasaur... I... kill you :(' do
+    before do
+      delete '/api/v1/pokemon/1'
+    end
+
+    it 'should respond with Bulbasaurs deleted data' do
+      expect(last_response.body).to eq(bulbasaur.to_json)
+    end
+  end
+
+  ############################################################
+  # POST test
+  ############################################################
   context 'Suki, I create you!' do
     before do
       post '/api/v1/pokemon', pokemon: suki
     end
 
-    it 'should respond with Bulbasaurs updated data' do
+    it 'should respond with Sukis created data' do
       response = JSON.parse last_response.body
       response.delete 'id'
 
